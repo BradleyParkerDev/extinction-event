ebbd1d6563b90a6eb6e469ec3e046e26b2566f8b
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-11-02 10:57:52 -0500

    updated

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-let orderedListItem = document.querySelectorAll('#ordered li')
+let orderedListItem = document.querySelectorAll('#ordered li');
 
 for(let i = 0; i < orderedListItem.length; i++){
     orderedListItem[i].addEventListener('click',function(){
         
-        orderedListItem[i].style.textDecoration = "line-through"
-        console.log('Ordered list item crossed out!')
+        orderedListItem[i].style.textDecoration = "line-through";
+        console.log('Ordered list item crossed out!');
     })
 }
+
+let unorderedListItem = document.querySelector('#unordered');
+
+unorderedListItem.addEventListener('click', function(event){
+    let listItem = event.target;
+    console.log(listItem.innerText);
+    listItem.style.opacity ='0';
+
+})
+
+let meteor = document.querySelector('#destroy-all');
+
+let rowImage = document.querySelectorAll('#row img')
+meteor.addEventListener('click',function(){
+
+    for(let i = 0; i < rowImage.length; i++){
+        rowImage[i].style.width ='0px';
+    }
+    console.log("Images disapeared!");
+})
